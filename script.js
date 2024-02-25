341bec5b6abd22626a1b5140784d46e832d22fc9
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-25 22:15:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+function promiseCreator(url){
+	return new Promise((res , rej) => {
+		let img = new Image();
+		img.onload = function (){
+			res(img);
+		};
+		img.onerror = function(){
+			rej(new Error(`Failed to load image's URL: ${url}`));
+		}
+		img.src = url;
+	})
+}
+btn.addEventListener('click' , (e)=>{
+	const promises = images.map(ele => promiseCreator(ele.url));
+	Promise.all(promises)
+		.then( images => {
+			images.forEach( (image) => {
+				output.appendChild(image);
+			})
+		}).catch(e => {
+			console.error(e);
+		});
+});
